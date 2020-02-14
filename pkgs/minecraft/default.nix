cd5f6bdd626e1d6e6c96881114b7c8b60a1467b0
Sam Lakerveld <me@dark.red>
2020-02-15 00:16:26 +0100

    minecraft: update to 20w07a

--- pkgs/minecraft/default.nix
+++ pkgs/minecraft/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, jre }:
 stdenv.mkDerivation {
   pname = "minecraft-server";
-  version = "1.16-20w06a";
+  version = "1.16-20w07a";
 
   src = fetchurl {
-    url    = "https://launcher.mojang.com/v1/objects/9b74998642553efe0e4d1aa079dc737b3e4cdc13/server.jar";
-    sha256 = "1qg7dk5vkqhkn7njjq9prvlvb0haw7k0gzjpkp9qfvmpbwcygpnb";
+    url    = "https://launcher.mojang.com/v1/objects/3944965e1621a5ccbe99292479cc62e07bccd611/server.jar";
+    sha256 = "1xajd2hjrzv5jz9aqbsif46azib8vm2fwwk7958z5dvd5p7z1phn";
   };
 
   preferLocalBuild = true;

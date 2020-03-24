c6aad8651b704b8a7cac84d514bee45157585bca
Sam Lakerveld <me@dark.red>
2020-03-24 06:34:14 +0100

    add wasm2js

--- default.nix
+++ default.nix
@@ -2,10 +2,12 @@
 
 {
   overlay = self: super: {
-    brightctl                 = super.callPackage ./pkgs/brightctl { };
-    factorio-headless         = super.callPackage ./pkgs/factorio { };
+    brightctl                 = super.callPackage ./pkgs/brightctl {};
+    factorio-headless         = super.callPackage ./pkgs/factorio {};
     starbound                 = super.callPackage ./pkgs/starbound { inherit nonredistKey; };
-    minecraft-server-snapshot = super.callPackage ./pkgs/minecraft { };
+    minecraft-server-snapshot = super.callPackage ./pkgs/minecraft {};
+
+    nodePackages = (super.nodePackages or {}) // import ./pkgs/nodePackages {};
   };
 
   modules = {

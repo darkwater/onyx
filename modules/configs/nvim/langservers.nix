23ec58dd962655c6d02014af0603b32090e7cd7b
Sam Lakerveld <me@dark.red>
2020-06-24 12:53:19 +0200

    modules/config/nvim: update rust-analyzer

--- modules/configs/nvim/langservers.nix
+++ modules/configs/nvim/langservers.nix
@@ -9,10 +9,10 @@ let
 in {
   rust-analyzer = callPackage rust-analyzer-pkgs rec {
     inherit (unstable) rustPlatform;
-    rev = "2020-06-15";
+    rev = "2020-06-22";
     version = "unstable-${rev}";
-    sha256 = "1qwkhzhgw6sap6vf5ilzr96a88r3snfrf3fcfkzw3n67j7lvsprf";
-    cargoSha256 = "0gcgfgrrjxzcgdci709dvx3904sbqx03w4pkbj89kk44j65cdzsy";
+    sha256 = "1cxsdc4b1823i5dx7nvh584araqbhpj8lx3jc0cc8qgm9hdbphz8";
+    cargoSha256 = "0sr85grdgb5ilmpikaf56bpwrgd1xjya9cv4l37sbd7p6lka9y4c";
     doCheck = false;
   };
 }

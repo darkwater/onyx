b6f2c7d46a5842605c3e21f2850f976258c8111b
Sam Lakerveld <me@dark.red>
2020-06-24 12:47:20 +0200

    release: add onyx-nvim-pack

--- release.nix
+++ release.nix
@@ -1,1 +1,7 @@
-(import ./default.nix {}).overlay {} (import <nixpkgs> { config.allowUnfree = true; })
+let
+  onyx = (import ./default.nix {});
+  pkgs = import <nixpkgs> { config.allowUnfree = true; };
+in {
+  onyx-nvim-pack = (pkgs.callPackage ./modules/configs/nvim/pack.nix {});
+}
+// (onyx.overlay {} pkgs)

459ff9cf8806a12a50d35515a9a901792202ea4c
Sam Lakerveld <dark@dark.red>
2020-07-22 20:06:26 +0200

    update unstable

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 {
   # we use unstable for nvim plugins
   # see https://hydra.nixos.org/job/nixpkgs/trunk/unstable/all for which commits succeed
-  unstable = import (builtins.fetchTarball https://github.com/nixos/nixpkgs/archive/c323751a71e.tar.gz) {};
+  unstable = import (builtins.fetchTarball https://github.com/nixos/nixpkgs/archive/b8c367a7bd0.tar.gz) {};
 
   overlay = self: super: rec {
     brightctl                 = self.callPackage ./pkgs/brightctl {};

001ba1e876f8352f69493fd8fff7c5741c9d2992
Sam Lakerveld <dark@dark.red>
2020-09-12 20:24:52 +0200

    Revert "pkgs: add jcdriver"
    This reverts commit 2536c78fa1d7550e314ab72dc13f2d09ded8110b.
    
    it never actually worked and i was drunk

--- default.nix
+++ default.nix
@@ -12,7 +12,6 @@
     cargo-node                = self.callPackage ./pkgs/cargo-node {};
     crc16                     = self.callPackage ./pkgs/crc16 {};
     itm-tools                 = self.callPackage ./pkgs/itm-tools {};
-    jcdriver                  = self.callPackage ./pkgs/jcdriver {};
     onyx-nvim-pack            = self.callPackage ./pkgs/onyx-nvim-pack {};
     pjstore                   = self.callPackage ./pkgs/pjstore {};
     polybar                   = self.callPackage ./pkgs/polybar {};

1bfae62a20006127e2e531e1a85ca2354a598d67
Sam Lakerveld <dark@dark.red>
2020-09-12 16:29:26 +0200

    shell.nix: add onvim command

--- shell.nix
+++ shell.nix
@@ -1,16 +1,32 @@
 with import <nixpkgs> {
   overlays = [ (import ./default.nix {}).overlay ];
 };
-stdenv.mkDerivation {
+
+let
+  build = (writeShellScriptBin "build" ''
+    pkg="$1"
+    shift
+    nix-build -E "
+      with import <nixpkgs> {
+        overlays = [ (import ./default.nix {}).overlay ];
+      };
+      $pkg
+    " "$@"
+  '');
+in stdenv.mkDerivation {
   name = "onyx_overlay_shell";
   buildInputs = [
-    (writeShellScriptBin "build" ''
-      nix-build -E "
-        with import <nixpkgs> {
-          overlays = [ (import ./default.nix {}).overlay ];
-        };
-        $1
-      "
+    build
+
+    (writeShellScriptBin "onvim" ''
+      result="$(${build}/bin/build onyx-nvim-pack --no-out-link)"
+      exec ${neovim}/bin/nvim --cmd "set packpath=$result/share/nvim/site" "$@"
     '')
   ];
+
+  shellHook = ''
+    echo "commands:"
+    echo "  build <pkg>  - build the specified package"
+    echo "  onvim        - run nvim with onyx configuration"
+  '';
 }

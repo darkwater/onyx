2782d1fd8fcf91cf904b8248a7a206b3b2344792
Sam Lakerveld <dark@dark.red>
2023-01-20 14:56:10 +0100

    add summary

--- flake.nix
+++ flake.nix
@@ -6,38 +6,86 @@
     hermes.url = "github:darkwater/hermes";
   };
 
-  outputs = attrs @ { self, nixpkgs, ... }: {
-    overlays.default = final: prev: {
-      shino = attrs.shino;
-      hermes = attrs.hermes;
+  outputs = attrs @ { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in
+    rec {
+      overlays.default = final: prev: {
+        shino = attrs.shino;
+        hermes = attrs.hermes;
+      };
+
+      packages.x86_64-linux = {
+        shino = attrs.shino.defaultPackage.x86_64-linux;
+        hermes = attrs.hermes.defaultPackage.x86_64-linux;
+      };
+
+      nixosModules.default = import modules/onyx.nix;
+
+      nixosConfigurations.sinon = lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = attrs;
+        modules = [
+          modules/onyx.nix
+
+          nixos/sinon
+          nixos/common.nix
+          nixos/fbk-red-ssl.nix
+          nixos/wireguard.nix
+
+          ({ ... }: {
+            networking.hostName = "sinon";
+            onyx.shell.hostnameColor = {
+              fg = [ 196 252 227 ];
+              bg = [ 48 42 3 ];
+            };
+          })
+        ];
+      };
+
+      summary =
+        let
+          packages = host:
+            with lib;
+            with builtins;
+            concatStrings
+              (lists.unique
+                (sort lessThan
+                  (map
+                    (pkg: "  ${pkg.meta.name} - ${pkg.outPath}\n")
+                    nixosConfigurations."${host}".config.environment.systemPackages)));
+
+          # services = host:
+          #   with lib;
+          #   with builtins;
+          #   concatStrings
+          #     (lists.unique
+          #       (sort lessThan
+          #         (mapAttrsToList
+          #           (name: service:
+          #             if service ? enable
+          #               && service.enable.type.description == "boolean"
+          #               && service.enable.value
+          #             then
+          #               let
+          #                 package =
+          #                   if service ? package
+          #                     && service.package.type.description == "package"
+          #                   then service.package.value.meta.name
+          #                   else "-";
+          #               in
+          #               "  services.${name} pkg=${package}\n"
+          #             else "")
+          #           nixosConfigurations."${host}".options.services)));
+
+          hosts = lib.concatStrings (lib.mapAttrsToList
+            (host: config: ''
+              ${host}:
+              ${packages host}
+            '')
+            nixosConfigurations);
+        in
+        hosts;
     };
-
-    packages.x86_64-linux = {
-      shino = attrs.shino.defaultPackage.x86_64-linux;
-      hermes = attrs.hermes.defaultPackage.x86_64-linux;
-    };
-
-    nixosModules.default = import modules/onyx.nix;
-
-    nixosConfigurations.sinon = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = attrs;
-      modules = [
-        modules/onyx.nix
-
-        nixos/sinon
-        nixos/common.nix
-        nixos/fbk-red-ssl.nix
-        nixos/wireguard.nix
-
-        ({ ... }: {
-          networking.hostName = "sinon";
-          onyx.shell.hostnameColor = {
-            fg = [ 196 252 227 ];
-            bg = [ 48 42 3 ];
-          };
-        })
-      ];
-    };
-  };
 }

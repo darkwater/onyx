040d02e8d78e2d3772cb2ad8aae5ca2fa726f638
Sam Lakerveld <darkwater124@gmail.com>
2024-06-10 21:03:35 +0200

    ssh-keys: add shino yuugure

--- ssh-keys.nix
+++ ssh-keys.nix
@@ -9,6 +9,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID74Nk4s41TgaRqL/BBn44wtu666KME4edMVHmcdKESK deck@holo"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILzaBU2sJeN8IYnxTt4UVtUMSszpR9qreLG7WUelezRe dark@tetsuya"
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIMKGwKgho6kTn896nw5y3bf9MTNzHDH1JyclvLK7FuxHyl23Zdm7kNR9MdfBOvhH0INfpYTcVyYyKhjf/LVTZo= termius@yuugure"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDC3MX9s9PXNel1N4Mwdt8NHxjBOyAwQvkuN/Epp3VUH shino yuugure"
   ];
 
   arena = [
